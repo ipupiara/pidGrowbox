19867affca5571692d41092653a78393a017d9dc
Peter <kwarahi@gentlemansclub.de>
2019-06-15 07:15:32 +0200

    circular buffered printin so far running ok

--- gitAddFiles.bat
+++ gitAddFiles.bat
@@ -5,4 +5,7 @@ git add gitAddFiles.bat
 git add pidGrowbox/pidGrowbox.cproj
 git add tools/*
 git add pidGrowbox/pidGrowbox.mdl
+echo "STATUS:"
 git status
+echo "git diff --name-status master origin/master :"
+git diff --name-status master origin/master
